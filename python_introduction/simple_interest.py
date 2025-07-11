b37370e9d763b34fb1957a121daf5b479c7f377c
DENNIS MURUGA GACHURU <143262632+MuchaiG@users.noreply.github.com>
2025-07-11 20:41:55 +0300

    Update simple_interest.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -7,5 +7,5 @@ principal = 1000 #representing $1000
 rate = 0.05 #representing 5% annual interest rate
 time = 3 #representing 3 years
 
-I = (principal * rate * time)
-print("The simple interest is: ", I)
+interest = principal * rate * time
+print("The simple interest is: ", interest)
