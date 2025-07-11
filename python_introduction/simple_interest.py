905885410aa2c8a5cac35632eeee17808449cfce
DENNIS MURUGA GACHURU <143262632+MuchaiG@users.noreply.github.com>
2025-07-11 20:38:12 +0300

    Update simple_interest.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,6 +1,11 @@
+#Formula for simple interest is (I = P * R * T)
+#I = interest
+#P = principal
+#T = rate
+
 principal = 1000 #representing $1000
 rate = 0.05 #representing 5% annual interest rate
 time = 3 #representing 3 years
 
-interest = (principal * rate * time)
-print("The simple interest is: ", interest)
+I = (principal * rate * time)
+print("The simple interest is: ", I)
