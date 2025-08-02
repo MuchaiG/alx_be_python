c2863460ae9017df2a38b3e052651c89b72217f5
MuchaiG <dennisgachuru2@gmail.com>
2025-08-02 11:52:48 +0300

    Fix: Syntax Error

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -1,5 +1,3 @@
-# test_simple_calculator.py
-
 import unittest
 from simple_calculator import SimpleCalculator
 
@@ -15,13 +13,13 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.subtraction(10, 4), 6)
         self.assertEqual(self.calc.subtraction(0, 5), -5)
 
-    def test_multiply(self):
+    def test_multiplication(self):
         self.assertEqual(self.calc.multiply(3, 7), 21)
         self.assertEqual(self.calc.multiply(-1, 4), -4)
 
     def test_division(self):
-        self.assertEqual(self.calc.division(10, 2), 5)
-        self.assertEqual(self.calc.division(9, 3), 3)
+        self.assertEqual(self.calc.divide(10, 2), 5)
+        self.assertEqual(self.calc.divide(9, 3), 3)
 
     def test_divide_by_zero(self):
         result = self.calc.division(10, 0)
